a656157c15d4c847bff784112eb353c9c3c71a84
Petar Popovic <petarpopovic013@gmail.com>
2026-03-10 13:51:27 +0100

    speed test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const montserrat = Montserrat({
   subsets: ["latin", "latin-ext"],
@@ -41,6 +42,7 @@ export default function RootLayout({
     <html lang="sr" className={montserrat.variable}>
       <body className="font-body antialiased">
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
